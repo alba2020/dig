ba86aed896d51ec160642e92fe81e8a1e855be5d
Alba <alba2020@gmail.com>
2020-01-07 09:22:55 +0300

    1

--- ex3.js
+++ ex3.js
@@ -10,12 +10,20 @@ var minimist = require("minimist")
 var Transform = require("stream").Transform
 var zlib = require("zlib")
 
-
+var CAF = require("caf")
 
 var args = minimist(process.argv.slice(2), {
     boolean: ["help", "in", "out", "compress", "uncompress"],
     string: ["file"]
 })
+
+processFile = CAF(processFile)
+
+function streamComplete(stream) {
+    return new Promise(function c(res) {
+        stream.on("end", res)
+    })
+}
 
 var BASE_PATH = path.resolve(process.env.BASE_PATH || __dirname)
 var OUTFILE = path.join(BASE_PATH, "out.txt")
@@ -28,17 +36,29 @@ if (args.help) {
     printHelp()
 } else if (args.in || args._.includes("-")) {
     // getStdin().then(processFile).catch(error)
-    processFile(process.stdin)
+
+    let tooLong = CAF.timeout(25, "Took too long!")
+
+    processFile(tooLong, process.stdin)
+    .catch(error)
+
 } else if (args.file) {
     let stream = fs.createReadStream(path.join(BASE_PATH, args.file))
-    processFile(stream)
+
+    let tooLong = CAF.timeout(25, "Took too long!")
+
+    processFile(tooLong, stream)
+    .then(function() {
+        console.log("Complete!")
+    })
+    .catch(error)
 } else {
     error("Incorrect usage.", true)
 }
 
 // sleep(100)
 
-function processFile(inStream) {
+function *processFile(signal, inStream) {
     var outStream = inStream
 
     if (args.uncompress) {
@@ -68,7 +88,15 @@ function processFile(inStream) {
     } else {
         targetStream = fs.createWriteStream(OUTFILE)
     }
+
     outStream.pipe(targetStream)
+
+    signal.pr.catch(function f() {
+        outStream.unpipe(targetStream) 
+        outStream.destroy()
+    })
+
+    yield streamComplete(outStream)
 }
 
 function error(msg, includeHelp = false) {
@@ -79,8 +107,8 @@ function error(msg, includeHelp = false) {
 }
 
 function printHelp() {
-    console.log("ex2 usage:")
-    console.log("ex2.js --help")
+    console.log("ex3 usage:")
+    console.log("ex3.js --help")
     console.log("--help            print this help")
     console.log("--file={FILENAME} process the file")
     console.log("--in, -           process stdin")
